162d234451930ee950d40d582d7bba8f541e4be4
Nick Terner <nick@cooladata.com>
2013-05-13 19:33:28 +0300

    have app.yaml auto-generated given several asusmption
    - assume setting contain BASE_DIR and ENV_NAME
    - assume app.template.yaml template present present

--- rocket_engine/__init__.py
+++ rocket_engine/__init__.py
@@ -87,8 +87,35 @@ def path_appendine_sdk():
         site.addsitedir(os.path.join(PROJECT_DIR, 'appengine_libs'))
 
 
+def setup_env():
+    """
+    assume we are running with the correct setting - some commands will enforce
+    explicit --setting.
+
+    Always generate a new app.yaml by instanciating the template
+    """
+    import subprocess
+    from django.conf import settings
+
+    from django.template import Template, Context
+
+    version = subprocess.check_output(['git', 'describe'])
+    with open(os.path.join(settings.BASE_DIR, 'VERSION'), 'w') as f:
+        f.write(version)
+    context = {
+            'env_name': settings.ENV_NAME,
+            'version': version.replace('.', '-').replace('/', '-'),
+            'tmp': os.environ.get('TMP', ''),
+            }
+    with open(os.path.join(settings.BASE_DIR, 'app.template.yaml'), 'r') as template:
+        with open(os.path.join(settings.BASE_DIR, 'app.yaml'), 'w') as target:
+            target.write(Template(template.read()).render(Context(context)))
+
+
+
 if not on_appengine_remote:
     setup_appendine_sdk()
 path_appendine_sdk()
+setup_env()
 
 wsgi = WSGIHandler()
